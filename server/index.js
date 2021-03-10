3f31c69f7ebbc49c33f2029e07cea55af0803bd0
Ana Nikolova <ani.nik19@gmail.com>
2021-03-10 18:05:11 +0100

    redux continued

--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ const app = express();
 
 var postRoutes = require("./routes/posts");
 
-app.use("/posts", postRoutes)
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use("/posts", postRoutes)
 
 const CONNECTION_URL="mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.jgd3a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
